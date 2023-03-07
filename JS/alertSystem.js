b5536cdf087db5b6b1d7df4d04b9948d0e312f00
Sebastian1307 <56195983+Sebastian1307@users.noreply.github.com>
2023-03-06 19:23:32 -0500

    Add files via upload

---
+++ JS/alertSystem.js
@@ -0,0 +1,3 @@
+function ventanaEmerg(){
+    alert("Proximamente")
+}
---
+++ JS/easter.js
@@ -0,0 +1,8 @@
+function egg(){
+    alert("Felicidades encontraste un easter egg");
+    // Simulate a mouse click:
+    window.location.href = "/HTML/egg.html";
+
+    // Simulate an HTTP redirect:
+    window.location.replace("/HTML/egg.html");
+}
